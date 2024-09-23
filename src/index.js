d4a9b6c70376629090cdb5ba998deb0c40a1b3b5
sshnuke3 <70014578+sshnuke3@users.noreply.github.com>
2024-09-23 08:19:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.warhammer.tech": dockerHub,
+  "quay.warhammer.tech": "https://quay.io",
+  "gcr.warhammer.tech": "https://gcr.io",
+  "k8s-gcr.warhammer.tech": "https://k8s.gcr.io",
+  "k8s.warhammer.tech": "https://registry.k8s.io",
+  "ghcr.warhammer.tech": "https://ghcr.io",
+  "cloudsmith.warhammer.tech": "https://docker.cloudsmith.io",
+  "ecr.warhammer.tech": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.warhammer.tech": dockerHub,
 };
 
 function routeByHosts(host) {
